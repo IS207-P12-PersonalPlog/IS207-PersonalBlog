98485461ef639ab04f3585c1644c37bfc7df8931
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-11-03 11:42:11 +0700

    Add button to scroll to top

---
+++ app.js
@@ -0,0 +1,25 @@
+//Scroll to the top
+let mybutton = document.getElementById("scroll-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) 
+    {
+        mybutton.style.display = "block";
+    } 
+    else 
+    {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
