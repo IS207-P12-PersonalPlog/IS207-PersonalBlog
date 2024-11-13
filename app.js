4697ce21c1822156286c026bbcc464e022d173c7
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-11-13 10:45:33 +0700

    Scroll to the top of page when click on button

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
+
+
+// hide or show button when scroll
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 600) {
+    $('.scr_btn').fadeIn();
+  }
+  else {
+    $('.scr_btn').fadeOut();
+  } 
+});
+
+$(document).ready(function() {
+  // scroll to top when refresh page
+  $('html, body').animate({scrollTop: 0}, 'fast');
+  // scroll to top page when click button home or button scroll
+  $('.scrollup').click(function() {
+      $('html, body').animate({scrollTop : 0}, 600);
+      return false;
+  });
+});
